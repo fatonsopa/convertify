42d375dda7f304c7677c471ee557d331836b8117
Faton Sopa <faton.sopa@manaferra.com>
2020-06-18 08:54:46 +0200

    0.5 deployed

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'convertify',
   packages = ['convertify'],
-  version = '0.4',
+  version = '0.5',
   license='MIT',
   description = 'Convert images to a different format',
   author = 'Faton Sopa',
   author_email = 'faton.sopa@manaferra.com',
   url = 'https://github.com/fatonsopa/convertify',
-  download_url = 'https://github.com/fatonsopa/convertify/blob/master/dist/convertify-0.4.tar.gz',
+  download_url = 'https://github.com/fatonsopa/convertify/blob/master/dist/convertify-0.5.tar.gz',
   keywords = ['pythonforseo', 'convert-image', 'image-to-webp','images','page-speed'],
   install_requires=[
           'Pillow'
